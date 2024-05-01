407cba8a2d81ce21293552cf339ede589837b451
Soham <sohamkelaskar@gmail.com>
2024-05-01 23:46:19 +0530

    Side NavBar added

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -1,9 +1,89 @@
+import { BottomSection } from "@/components/bottom-section";
+import { NavItem, NavSection, NavSectionItems } from "@/components/nav-section";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
 }
 
+const pipelines: NavSectionItems = {
+    label: "Pipelines",
+    icon: "layers",
+    items: [
+        {
+            label: "Text Recognition",
+            href: "/text-recognition",
+            icon: "textSelect",
+        },
+        {
+            label: "Data Extraction",
+            href: "/data-extraction",
+            icon: "braces",
+        },
+        {
+            label: "Verfication",
+            href: "/verification",
+            icon: "checkCircle",
+        },
+    ],
+}
+
+const organizeData: NavSectionItems = {
+    label: "Organized Data",
+    icon: "grid",
+    items: [
+        {
+            label: "Receipts",
+            href: "/receipts",
+            icon: "receipt",
+        },
+        {
+            label: "Invoices",
+            href: "/invoices",
+            icon: "invoice",
+        },
+        {
+            label: "Card Statements",
+            href: "/card-statements",
+            icon: "creditCard",
+        },
+    ],
+}
+
+const bottomItems: NavItem[] = [
+    {
+      label: "Help",
+      href: "/help",
+      icon: "help",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+      icon: "settings",
+    },
+  ];
+
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-    return <div className="min-h-screen">{children}</div>;
+    return <div className="min-h-screen">
+        {/* SlideBarNav */}
+        <div className="flex flex-col fixed z-50 inset-y-0 w-72">
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r-2 border-slate-200 bg-white pl-8 pr-6 pb-4">
+                {/* Logo */}
+                <Link href="/dashboard">
+                    <Image className="flex mt-8 shrink-0" priority src="/logo.png" width={102} height={38} alt="Organise Simple Logo" />
+                </Link>
+                {/* Navigation */}
+                <nav className="flex flex-1 flex-col">
+                    <NavSection className="mt-20" section={pipelines} />
+                    <NavSection className="mt-10" section={organizeData} />
+                    <div className="flex flex-1 flex-col gap-y-7">
+                        <BottomSection className="mt-auto" username="Soham" items={bottomItems}/>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        {children}
+    </div>;
 }
--- frontend/components/auth-form.tsx
+++ frontend/components/auth-form.tsx
@@ -6,11 +6,11 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react"
 import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { buttonVariants } from "@/components/ui/button"
+import { Label } from "../@/components/ui/label"
+import { Button } from "../@/components/ui/button"
+import { buttonVariants } from "../@/components/ui/button"
 import { Icons } from "./icons"
+import { Input } from "../@/components/ui/input"
 
 
 interface AuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
@@ -34,12 +34,30 @@ export function AuthForm({ className, ...props }: AuthFormProps) {
             <div className="grid gap-6">
                 <div className="grid gap-1.5">
                     <Label htmlFor="username">Username</Label>
-                    <Input id="username" type="text" placeholder="Username" autoCapitalize="none" autoComplete="text" autoCorrect="off" disabled={isLoading} {...register("username")} />
+                    <Input
+                        id="username"
+                        type="text"
+                        placeholder="Username"
+                        autoCapitalize="none"
+                        autoComplete="text"
+                        autoCorrect="off"
+                        disabled={isLoading}
+                        style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '0.5rem' }}
+                        {...register("username")}
+                    />
                     {errors?.username && (<p className="text-red-500 text-xs px-1">{errors.username.message}</p>)}
                 </div>
                 <div className="grid gap-1.5">
                     <Label htmlFor="password">Password</Label>
-                    <Input id="password" type="text" autoCapitalize="none" autoCorrect="off" disabled={isLoading} {...register("password")} />
+                    <Input
+                        id="password"
+                        type="text"
+                        autoCapitalize="none"
+                        autoCorrect="off"
+                        disabled={isLoading}
+                        style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '0.5rem' }}
+                        {...register("password")}
+                    />
                     {errors?.password && (<p className="text-red-500 text-xs px-1">{errors.password.message}</p>)}
                 </div>
                 <Button className={cn("mt-2", buttonVariants({
