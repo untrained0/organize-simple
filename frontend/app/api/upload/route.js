602e24a40c8d1f0fdd31ac6ffe5cdc6c5f046384
Soham <sohamkelaskar@gmail.com>
2024-05-13 23:41:45 +0530

    extraction table schema added

--- frontend/app/api/upload/route.js
+++ frontend/app/api/upload/route.js
@@ -1,6 +1,10 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { randomUUID } from "crypto";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import prisma from "@/lib/prisma";
 
 export async function POST(req) {
   const session = await getServerSession(authOptions);
@@ -10,9 +14,11 @@ export async function POST(req) {
   }
 
   const contentType = req.headers.get("content-type");
-  if (!contentType.includes("multipart/form-data")) {
+  if (!contentType?.includes("multipart/form-data")) {
     return NextResponse.json(
-      { error: "Invalid Content-Type" },
+      {
+        error: "Content type must be multipart/form-data",
+      },
       { status: 400 }
     );
   }
@@ -20,15 +26,75 @@ export async function POST(req) {
   try {
     const formData = await req.formData();
     const file = formData.get("file");
+
     if (!file) {
-      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
     }
 
-    return NextResponse.json({ message: "File Upload" }, { status: 201 });
+    const blob = file;
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const s3 = new S3Client({
+      region: process.env.S3_REGION,
+      endpoint: process.env.S3_ENDPOINT,
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      },
+      forcePathStyle: true,
+    });
+
+    const userUUID = session?.user.id;
+    const fileUUID = randomUUID();
+
+    const s3Params = {
+      Bucket: process.env.S3_BUCKET,
+      Key: `${userUUID}/${fileUUID}`,
+      Body: buffer,
+      ContentType: blob.type,
+    };
+
+    const signedUrl = await getSignedUrl(s3, new PutObjectCommand(s3Params), {
+      expiresIn: 3600,
+    });
+
+    const res = await fetch(signedUrl, {
+      method: "PUT",
+      body: buffer,
+      headers: {
+        "Content-Type": "application/pdf",
+      },
+    });
+
+    if (!res.ok) {
+      return NextResponse.json(
+        {
+          error: "Could not upload file",
+        },
+        { status: 500 }
+      );
+    }
+
+    const extraction = await prisma.extraction.create({
+      data: {
+        filename: blob.name,
+        objectPath: s3Params.Key,
+        user: {
+          connect: { id: userUUID },
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: { id: extraction.id, filename: blob.name },
+      },
+      { status: 201 }
+    );
   } catch (error) {
     return NextResponse.json(
       {
-        error: error.message,
+        error: "Could not parse content as FormData",
       },
       { status: 400 }
     );
